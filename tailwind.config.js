a4f3b09ada96d9900f10a22980adf3e6297148a5
BROD NICKY <coffedrop@live.com>
2023-09-27 23:22:11 -0400

    Finalize colour scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,32 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        blue: {
+          50: '#67B3E0',
+          100: '#2684BA',
+          200: '#2274A5',
+          300: '#1D6690',
+          700: '#091F67',
+          800: '#071952',
+          900: '#051138'
+        },
+        magenta: {
+          100: '#BC1058',
+          200: '#A40E4C',
+          300: '#8D0C42'
+        },
+        coral: {
+          100: '#F6A9A7',
+          200: '#F49390',
+          300: '#F17D79'
+        },
+        pink: {
+          100: '#F66F7A',
+          200: '#F45866',
+          300: '#F33F4E'
+        }
+      }
     },
   },
   plugins: [],
