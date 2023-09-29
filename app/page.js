51e667879eb0bad54bef8c78547d24c9b51aa585
BROD NICKY <coffedrop@live.com>
2023-09-29 17:07:52 -0400

    Reorganize landing page

--- app/page.js
+++ app/page.js
@@ -2,9 +2,9 @@ import Button from '@/components/Button';
 
 export default function Home() {
   return (
-    <main className='p-12 min-h-screen grid place-items-center grid-rows-1 grid-cols-1 md:grid-cols-12'>
+    <main className='min-h-screen p-12 grid place-items-center grid-rows-1 grid-cols-1 md:grid-cols-12'>
       <div className='flex flex-col gap-8 md:col-span-7'>
-        <h1 className='text-6xl font-bold text-center md:text-7xl md:text-left'>
+        <h1 className='text-center md:text-7xl md:text-left'>
           Voting Made Easy
         </h1>
         <p className='text-lg font-medium text-center md:text-xl md:text-left'>
@@ -14,8 +14,8 @@ export default function Home() {
           secure, and pseudonymous election or poll results.
         </p>
         <div className='flex flex-row justify-center gap-4 md:justify-start'>
-          <Button>Create a Poll</Button>
-          <Button>Submit a Vote</Button>
+          <Button href='/create'>Create a Poll</Button>
+          <Button>Submit a Poll</Button>
         </div>
       </div>
     </main>
